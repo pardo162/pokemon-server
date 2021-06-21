a20f3487fece09d122da60fd74f813f774e49271
alejandro <pardo162@hotmail.com>
2021-06-21 01:50:36 -0500

    prueba

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 
 // // Start writing Firebase Functions
+// // prueba
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.region("europe-west1")
