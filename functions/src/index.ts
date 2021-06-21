eed80e8a20a018c492edd927e0809eb7d5befccd
alejandro <pardo162@hotmail.com>
2021-06-21 01:39:25 -0500

    primera funcion

---
+++ functions/src/index.ts
@@ -0,0 +1,9 @@
+import * as functions from "firebase-functions";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions.region("europe-west1")
+    .https.onRequest((request, response) => {
+      response.send("Hello from Pokegotchi");
+    });
